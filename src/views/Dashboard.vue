10284f95981dd2a28a8270eb85c237685ec97321
yunpeng4456 <1441331786@qq.com>
2021-02-08 12:08:38 +0800

    试发布

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,11 +6,33 @@
             <img :src="origin+'/lovelijun/z.webp'"/>
             <img :src="origin+'/lovelijun/avatar2.jpg'" style="width: 60px; border-radius: 50%;">
         </div>
+        <div class="love-time">
+            <h2>我们相恋了<span style="color:red;">❤️</span></h2>️
+            <div class="timer">
+                <b>
+                    {{days}}
+                </b>
+                Days
+                <b>
+                    {{hours}}
+                </b>
+                Hours
+                <b>
+                    {{minutes}}
+                </b>
+                Minutes
+                <b>
+                    {{seconds}}
+                </b>
+                Seconds
+                
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-
+    import moment from 'moment'
     export default {
         name: "Dashboard",
         components: {
@@ -18,8 +40,12 @@
         },
         data() {
             return {
-                origin:location.origin
-
+                origin:'http://miao-yunpeng.gitee.io',//location.origin
+                days:0,
+                minutes:0,
+                hours:0,
+                seconds:0
+                
             };
         },
 
@@ -27,6 +53,11 @@
             console.log()
         },
         mounted() {
+           
+            
+            this.interval = setInterval(this.calcTime,1000);
+            
+            
         },
         computed: {
 
@@ -37,25 +68,61 @@
         watch: {},
 
         methods: {
-            //设置背景地图
+            //
+            calcTime(){
+                let t1= moment('2020-12-25 20:00:00');
+                
+                let t2 = moment();
+                
+                //计算时间差并转换成标准时间单位
+                
+                let dura = t2.format('x') - t1.format('x');
+                
+                let tempTime = moment.duration(dura);
+                this.days = t2.diff(t1, 'day')
+                this.hours = t2.diff(t1, 'hour')%24;
+                this.minutes = t2.diff(t1, 'minute')%60;
+                this.seconds = t2.diff(t1, 'second')%60;
+                
+            }
 
 
         },
 
         destroyed() {
-
+            if(this.interval){
+                clearInterval(this.interval)
+            }
         }
 
     };
 </script>
 
 <style lang="scss">
+    body{
+        -webkit-font-smoothing: antialiased;
+        font-family: Helvetica Neue, Helvetica, Hiragino Sans GB, Microsoft YaHei, Arial, sans-serif;
+    }
     .wrapper{
         transform: translate(0, -50%);
         text-align: center;
         position: relative;
         width: 100%;
         top: 150px;
+    }
+    .love-time{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
+    }
+    .timer{
+        font-size: 180%;
+        line-height: 1.5;
+        b{
+            color:rgb(253, 99, 125);
+        }   
     }
 
 </style>
